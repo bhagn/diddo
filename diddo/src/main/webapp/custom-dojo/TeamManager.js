20efb03c681acdacc5a127dbc95683c417b5e0ff
Bhagavan Nagaraj <bhagavan1@gmail.com>
2012-08-25 00:13:22 +0530

    added form controls

--- diddo/src/main/webapp/custom-dojo/TeamManager.js
+++ diddo/src/main/webapp/custom-dojo/TeamManager.js
@@ -1,4 +1,5 @@
-define(["dojo/_base/declare", "dojo/_base/xhr", "dojo/parser", "dojo/dom", "dojo/dom-construct", "dojo/ready", "dojo/on", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin", "dijit/layout/_LayoutWidget", "dijit/_Container", "dojo/text!./templates/TeamManager.html", "custom/DiddoRestUI", "custom/Team", "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dijit/form/Button"],
+define(["dojo/_base/declare", "dojo/_base/xhr", "dojo/parser", "dojo/dom", "dojo/dom-construct", "dojo/ready", "dojo/on", "dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin", "dijit/layout/_LayoutWidget", "dijit/_Container", "dojo/text!./templates/TeamManager.html", "custom/DiddoRestUI", "custom/Team", 
+        "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dijit/form/Button", "dijit/form/Form", "dijit/form/TextBox", "dijit/form/ValidationTextBox", "dijit/form/Select", "dijit/form/CheckBox"],
 		function(declare, xhr, parser, dom, domConstruct, ready, on, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, _LayoutWidget, _Container, template, RestUI, Team) {
 	return declare("TeamManager", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, _Container], {
 		templateString: template,
